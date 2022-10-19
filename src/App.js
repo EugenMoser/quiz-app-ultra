6dd36681eca7af312ee11e91068d7c85e58da5c1
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2022-10-19 12:37:09 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1></h1>
       </header>
+      <main>
+        <section>
+          <p></p>
+          <button> </button>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+
+        <section>
+          <p></p>
+          <button> </button>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+      </main>
+
+      <nav>
+        <button></button>
+        <button></button>
+        <button></button>
+      </nav>
     </div>
   );
 }
